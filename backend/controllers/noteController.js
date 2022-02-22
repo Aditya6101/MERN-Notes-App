b3739ab86c19ba1d3e7d39723f65e76a78d719bf
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-22 12:08:35 +0530

    functionality added.

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,35 +1,65 @@
 const asyncHandler = require('express-async-handler');
 
+const Note = require('../models/noteModel');
+
 // @desc    Get Notes
 // @route   GET /api/notes
 // @access  Private
-asyncHandler(async function getNotes(req, res) {
-  res.status(200).send({ msg: 'Get Notes' });
+const getNotes = asyncHandler(async (req, res) => {
+  const notes = await Note.find();
+
+  res.status(200).send(notes);
 });
 
 // @desc    Create Note
 // @route   POST /api/notes
 // @access  Private
-asyncHandler(async function createNote(req, res) {
+const createNote = asyncHandler(async (req, res) => {
   if (!req.body.title) {
     res.status(400);
     throw new Error('Please add text fields');
   }
-  res.status(200).send({ msg: 'Create Note' });
+
+  const { title, desc, text } = req.body;
+  const note = await Note.create({
+    title,
+    desc,
+    text,
+  });
+
+  res.status(201).send(note);
 });
 
 // @desc    Update Note
 // @route   PUT /api/notes/:id
 // @access  Private
-asyncHandler(async function updateNote(req, res) {
-  res.status(200).send({ msg: `update note ${req.params.id}` });
+const updateNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (!note) {
+    res.status(400);
+    throw new Error('Note not found');
+  }
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).send(updatedNote);
 });
 
 // @desc    Delete Note
 // @route   DELETE /api/notes/:id
 // @access  Private
-asyncHandler(async function deleteNote(req, res) {
-  res.status(200).send({ msg: `delete note ${req.params.id}` });
+const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (!note) {
+    res.status(400);
+    throw new Error('Note not found');
+  }
+  await note.remove();
+
+  res.status(200).send({ id: req.params.id });
 });
 
 module.exports = {
