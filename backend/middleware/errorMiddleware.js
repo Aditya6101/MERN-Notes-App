735d2da865ece5658659ed31202795ae6cc0e59a
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-22 09:57:20 +0530

    error middleware added.

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  res.status(statusCode);
+
+  res.send({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+  next();
+};
+
+module.exports = {
+  errorHandler,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/notes', require('./routes/noteRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
