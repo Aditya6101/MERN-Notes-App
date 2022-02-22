42409718542e2212a2fede2b1eac0fe2ce6493c4
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-22 12:07:29 +0530

    Note Model Added.

---
+++ backend/models/noteModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please add a title value'],
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    text: {
+      type: String,
+      required: [true, 'Please add a text value'],
+    },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Note', noteSchema);
