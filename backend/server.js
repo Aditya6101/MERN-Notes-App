00b28c71da55fd9a1908bb77b60383df44c967d1
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-22 09:27:43 +0530

    new middlwares added.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/notes', require('./routes/noteRoutes'));
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
