20b412427af1998fd1993bb3962987f9a953580a
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-22 09:26:08 +0530

    code shortened.

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getNotes,
+  createNote,
+  updateNote,
+  deleteNote,
+} = require('../controllers/noteController');
 
-router.get('/', (req, res) => {
-  res.status(200).send({ msg: 'Get Notes' });
-});
-
-router.post('/', (req, res) => {
-  res.status(200).send({ msg: 'Create Note' });
-});
-
-router.put('/:id', (req, res) => {
-  res.status(200).send({ msg: `update note ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).send({ msg: `delete note ${req.params.id}` });
-});
+router.route('/').get(getNotes).post(createNote);
+router.route('/:id').put(updateNote).delete(deleteNote);
 
 module.exports = router;
