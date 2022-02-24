3a1ce45ab47db664cb5e369811156314ee4fa01a
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-24 08:53:46 +0530

    user route & model added.

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const noteSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     title: {
       type: String,
       required: [true, 'Please add a title value'],
