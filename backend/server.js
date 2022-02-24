16ab126a7a4503b9bc30ee848ee901dff103a8ad
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-24 08:57:34 +0530

    userRoutes added.

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/notes', require('./routes/noteRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
