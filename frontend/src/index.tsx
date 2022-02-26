85cf11385fda5f5eba4da1d2d75e315045fe569c
Aditya Kamble <adityakamble6101@gmail.com>
2022-02-26 13:21:02 +0530

    tailwindcss init.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
